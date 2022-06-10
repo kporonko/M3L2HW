923d182ad1221e96d1cdb427025fb6b22188b587
KeeYoo <poronko2045@gmail.com>
2022-06-10 11:49:07 +0300

    Release

---
+++ Delegates/Delegates/Class2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Delegates.Class1;
+
+namespace Delegates
+{
+    internal class Class2
+    {
+        /// <summary>
+        /// Delegate for Pow operation field.
+        /// </summary>
+        private DelegateForPow _powDelegate;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Class2"/> class.
+        /// </summary>
+        public Class2()
+        {
+            // If we dont have any links in pow delegate - we must add new link on method Pow by method register.
+            if (PowDelegate == null)
+            {
+                Class1 class1 = new Class1();
+                Register(class1.Pow);
+            }
+        }
+
+        /// <summary>
+        /// Delegate for Result method.
+        /// </summary>
+        /// <param name="x">Value we want to check.</param>
+        /// <returns>True or false.</returns>
+        public delegate bool Predicate(int x);
+
+        /// <summary>
+        /// Gets result of the Pow operation and stores it.
+        /// </summary>
+        /// <value>
+        /// Result of the Pow operation.
+        /// </value>
+        public int Res { get; private set; }
+
+        /// <summary>
+        /// Gets or sets field (same story as with Class1 property and value.)
+        /// </summary>
+        /// <value>
+        /// Field.
+        /// </value>
+        internal DelegateForPow PowDelegate { get => _powDelegate; set => _powDelegate = value; }
+
+        /// <summary>
+        /// Method for operating with 2 variables by mathods, stored in delegate.
+        /// </summary>
+        /// <param name="delegateForPow">Delegate which methods we want to use on our x and y vars.</param>
+        /// <param name="x">First value.</param>
+        /// <param name="y">Second value.</param>
+        /// <returns>Delegate with Result method in it.</returns>
+        public Predicate Calc(DelegateForPow delegateForPow, int x, int y)
+        {
+            Res = delegateForPow.Invoke(x, y);
+            return new Predicate(Result);
+        }
+
+        /// <summary>
+        /// Method to check whether our Res value is divided entitely by x or no.
+        /// </summary>
+        /// <param name="x">Value.</param>
+        /// <returns>True or false.</returns>
+        public bool Result(int x)
+        {
+            if (Res % x == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Method to add a new method links to delegate for pow.
+        /// </summary>
+        /// <param name="handler">Delegate.</param>
+        public void Register(DelegateForPow handler)
+        {
+            PowDelegate += handler;
+        }
+    }
+}
--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using static Delegates.Class1;
 
 namespace Delegates
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Class1 class1 = new Class1();
+
+            class1.Delegate1 = Show;
+            Class2 class2 = new Class2();
+
+            var powRes = class2.Calc(class2.PowDelegate, 10, 7);
+            class1.Delegate1(powRes(5));
+            Console.ReadLine();
+        }
+
+        public static void Show(bool val)
+        {
+            Console.WriteLine(val);
         }
     }
 }
